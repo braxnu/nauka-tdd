e21d6cd0bd33d9597a396d3a405484aef7ecf46c
Karol Baczewski <braxnu@gmail.com>
2017-09-22 21:50:13 +0100

    Add second test

--- spec/pkn.spec.js
+++ spec/pkn.spec.js
@@ -3,5 +3,9 @@ var gra = require('../src/pkn')
 describe('Papier Kamien Nozyce', function () {
   it('gra przyjmuje trzy figury', function () {
     expect(gra.runda).toBeDefined();
+
+    expect(function () {
+      gra.runda("P", "N")
+    }).not.toThrow();
   })
 })
